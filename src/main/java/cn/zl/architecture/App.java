d05c58ddbd63dddd22963ee8f58cb72ff755a50f
wackycrazy <wackycrazy@163.com>
2018-08-03 23:00:26 +0800

    使用maven分模块、分工程管理

---
+++ src/main/java/cn/zl/architecture/App.java
@@ -0,0 +1,13 @@
+package cn.zl.architecture;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
